16f7e62b2e06fcf87fd09bfb1159696ae5a643df
Yevhen Bondar <eugenij.bondar@gmail.com>
2022-09-20 19:17:06 +0300

    add useFetch

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+   Kyiv Time: {{ timeData.utc_datetime }}
+  </div>
+</template>
+
+<script setup>
+const { data: timeData } = await useFetch('https://worldtimeapi.org/api/timezone/Europe/Kiev')
+</script>
